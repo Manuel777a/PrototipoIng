6b16d90e063c88739f76f5e221a230e23275f814
Manuel777a <36998380+Manuel777a@users.noreply.github.com>
2018-05-24 07:58:24 -0500

    Creacion interfaz Generacion de equipo
    Se genero la interfaz que crea el equipo

---
+++ Ingenieria Software Prototipo/Ingenieria Software Prototipo/GUIGeneracionEquipo.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ingenieria_Software_Prototipo
+{
+    public partial class GUIGeneracionEquipo : Form
+    {
+        public GUIGeneracionEquipo()
+        {
+            InitializeComponent();
+        }
+
+        private void btnConsultarPropuesta_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
